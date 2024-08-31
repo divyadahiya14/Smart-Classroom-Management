6f125b9a3677b002577141617c98f50ced8636e3
divyadahiya14 <divyadahiya147@gmail.com>
2024-08-31 17:29:15 +0530

    added professor result

--- FRONTEND/src/Pages/ProfessorResults.jsx
+++ FRONTEND/src/Pages/ProfessorResults.jsx
@@ -1,16 +1,30 @@
-import React from 'react'
+import React from 'react';
+import ProfessorSidebar from '../Components/ProfessorSidebar';
+import Topbar from '../Components/Topbar';
+import FilterBar from '../Components/FilterBar';
 
 const ProfessorResults = () => {
+    return (
+        <div className="flex w-screen h-screen">
+            {/* Sidebar */}
+            <ProfessorSidebar />
+            
+            {/* Main Content Area */}
+            <div className="w-[85%] bg-gradient-to-b from-purple-100 via-orange-50 to-transparent">
+                {/* Topbar */}
+                <Topbar />
+                
+                {/* Content Area */}
+                <div className="flex flex-col p-6 space-y-6">
+                    {/* Filter Bar with Upload Photo */}
+                    <FilterBar />
+                    
+                    {/* Professor Results or other content */}
+                    
+                </div>
+            </div>
+        </div>
+    );
+};
 
-//   if(!localStorage.getItem("token")){
-//     console.log("you are not logged in");
-//     return (<></>)
-// }
-  return (
-    <div>
-      ProfessorResults
-    </div>
-  )
-}
-
-export default ProfessorResults
+export default ProfessorResults;
